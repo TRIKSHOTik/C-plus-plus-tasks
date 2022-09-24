44be566c9be8b0e6ad1307249664b8bec6ce44c4
Alex <103889733+TRIKSHOTik@users.noreply.github.com>
2022-09-24 21:29:36 +0300

    Add files via upload

---
+++ task313/task313.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double h;
+    cout << "vvedi h - znachenie chaga = ";
+    cin >> h;
+    cout << " --------------------" << endl;
+    for (int i = -3; i + h <= 3; i += h) {
+        cout << (pow(i, 2) - 5 * i - 3) << endl;
+    }
+    cout << "---------------------" << endl;
+    return 0;
+}
