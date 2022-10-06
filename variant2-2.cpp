a35912508971888d6e339cfc0b2edc8d19ef44a8
Alex <103889733+TRIKSHOTik@users.noreply.github.com>
2022-10-06 21:24:33 +0300

    Add files via upload

---
+++ variant2-2.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double distance(double x1, double y1, double x2, double y2) {
+    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+}
+
+double H(double x1, double y1, double x2, double y2, double x3, double y3) {
+    double a = distance(x1, y1, x2, y2);
+    double b = distance(x2, y2, x3, y3);
+    double c = distance(x1, y1, x3, y3);
+    double p = (a + b + c) / 2;
+
+    return (2 / a) * sqrt(p * (p - a) * (p - b) * (p - c));
+}
+
+double min3(double x, double y, double z) {
+    if (x >= y && x >= z) {
+        return x;
+    }
+    else if (y >= x && y >= z) {
+        return y;
+    }
+    else {
+        return z;
+    }
+}
+
+int main() {
+    setlocale(0, "");
+
+    double x1, y1;
+    cout << "Координаты точки первой вершины тругольника: ";
+    cin >> x1 >> y1;
+
+    double x2, y2;
+    cout << "Координаты точки второй вершины тругольника: ";
+    cin >> x2 >> y2;
+
+    double x3, y3;
+    cout << "Координаты точки третьей вершины тругольника: ";
+    cin >> x3 >> y3;
+
+    double x, y;
+    cout << "Координаты произвольной точки, лежащей внутри треугольника: ";
+    cin >> x >> y;
+
+    double h1 = H(x1, y1, x2, y2, x, y);
+    double h2 = H(x2, y2, x3, y3, x, y);
+    double h3 = H(x1, y1, x3, y3, x, y);
+
+    cout << min3(h1, h2, h3);
+
+    return 0;
+}
---
+++ variant2.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+using namespace std;
+bool
+check(int x)
+{
+    if (x < 10)
+        return true;
+    int prevNum = x % 10;
+    int curNum = (x / 10) % 10;
+    bool bad = false;
+    while (x > 0)
+    {
+        if (prevNum <= curNum)
+        {
+            bad = true;
+            break;
+        }
+        x /= 10;
+        prevNum = x % 10;
+        curNum = (x / 10) % 10;
+    }
+    if (bad)
+    {
+        return false;
+    }
+    return true;
+}
+
+int
+main()
+{
+    int m, n, i;
+    cout << "n=";
+    cin >> n;
+    cout << "m=";
+    cin >> m;
+    bool p = false;
+    for (int i = n; i <= m; i++)
+        if (check(i))
+        {
+            cout << i << " ";
+            p = true;
+        }
+    if (!p)
+        cout << "net chisel" << endl;
+    return 0;
+}
