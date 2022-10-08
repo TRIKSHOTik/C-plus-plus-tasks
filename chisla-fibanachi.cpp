a4c4365fff06b77b9602b1ee084dde7cc80e0e01
Alex <103889733+TRIKSHOTik@users.noreply.github.com>
2022-10-08 20:42:40 +0300

    Add files via upload

---
+++ chisla-fibanachi.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int fibanachi(int x) {
+    if (x < 1)
+        return 0;
+    if (x == 1) {
+        return 1;
+    }
+    else {
+        return fibanachi(x - 1) + fibanachi(x - 2);
+    }
+
+}
+int main()
+{
+    int y;
+    int res = 0;
+    cout << "vvedi y = ";
+    cin >> y;
+    while (res < y) {
+        cout << fibanachi(res) << endl;
+        res++;
+    }
+    return 0;
+}
