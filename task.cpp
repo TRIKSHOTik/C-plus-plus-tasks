18b152c39b3bad4b23bd66ec3545760c6cf33089
Alex <103889733+TRIKSHOTik@users.noreply.github.com>
2022-11-07 14:27:44 +0300

    Add files via upload

---
+++ task.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main()
+{
+    ifstream fin("input.txt");
+    int n;
+    fin >> n;
+    string maxWord;
+    for (int i = 0; i < n; i++) {
+        string x;
+        fin >> x;
+        if (x.size() > maxWord.size()) {
+            maxWord = x;
+        }
+    }
+    cout << "Max slovo = " << maxWord << endl;
+}
